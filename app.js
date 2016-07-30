bc1927c7672e42485a4caf3f8e514870c5b0afbf
Sacha David <sach1810@gmail.com>
2016-07-30 12:03:54 +0300

    test

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+// var bodyParser = require('body-parser');
+var app = express();
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.get('/', function (request, response) {
+  // response.send("You are inside the fullstack project")
+});
+
+app.get('/hooks', function (req, res) {
+    console.log(req);
+    console.log(res);
+    res.send("hi");
+});
+
+app.listen(8000);
