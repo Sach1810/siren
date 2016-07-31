f0b9ba35428eb75c43274edd2c31590cd7992b13
Sacha David <sach1810@gmail.com>
2016-07-31 12:17:27 +0300

    changed body parser

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 // var bodyParser = require('body-parser');
 var app = express();
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
